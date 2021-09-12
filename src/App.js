42be109521648870fe652c82237f512a111e3f75
fatih <fatihenaz@gmail.com>
2021-09-12 18:48:39 +0300

    created app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Contacts from './components/contacts/Contacts';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Contacts />
+  
+    </div>
+  );
+}
+
+export default App;
