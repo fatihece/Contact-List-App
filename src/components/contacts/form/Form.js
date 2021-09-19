d9a11318e7a6d4691cf79b115fb4a04ddd9f3cb4
fatih <fatihenaz@gmail.com>
2021-09-19 15:42:26 +0300

    update form comp

--- src/components/contacts/form/Form.js
+++ src/components/contacts/form/Form.js
@@ -1,6 +1,7 @@
-import React, { useState }from 'react'
+import React, { useState } from 'react';
 
-const initialFormValues = {fullname:"", phone_number:""}
+const initialFormValues = { fullname: "", phone_number: "" }
+
 const Form = ({setContact, contact}) => {
     const[form,setForm] = useState(initialFormValues)
 
